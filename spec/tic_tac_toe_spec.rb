20cb32d1fdcf79d092c2a33d67b0fc68f0d2f7be
Bobby Iveson <bobby.iveson@Bobbys-MacBook-Pro.local>
2022-06-24 13:57:17 +0100

    Initial commit

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,166 @@
+require_relative '../tic_tac_toe'
+
+
+describe 'tictactoe game' do
+    it 'has a grid with width of 3 and a height of 3 meaning 3 * 3 = 9' do
+        grid = initialise_grid
+        expect(grid.length).to eq(9)
+    end
+
+    it 'returns grid with X at index 1 of grid if human player enters 2' do
+        grid = initialise_grid
+        expect(next_turn("player", grid, 2)).to eq(["", "X", "",
+                                                    "", "", "",
+                                                    "", "", ""])
+    end
+
+    it 'returns grid with O at index 1 of grid if computer player enters 2' do
+        grid = initialise_grid
+        expect(next_turn("computer", grid, 2)).to eq(["", "O", "",
+                                                      "", "", "",
+                                                      "", "", ""])
+    end
+
+    it 'if player enters number out of bounds not between 1 & 9 inclusive then ask user to try again' do
+        grid = initialise_grid
+        expect { next_turn("player", grid, 0) }.to output("invalid index try a number between 0-9\n").to_stdout
+    end
+
+
+    it 'if player enters number that has already been selected then tell user to try again' do
+        grid = initialise_grid
+        next_turn("player", grid, 1)
+        expect { next_turn("player", grid, 1) }.to output("invalid index this space is already taken try again\n").to_stdout
+    end
+
+    it 'if top row have all of the same X or Os then this is a winning game so return ' do
+        grid = [
+            "X", "X", "X",
+            "", "", "",
+            "", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "",
+            "X", "X", "X",
+            "", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "",
+            "", "", "",
+            "X", "X", "X"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "X", "", "",
+            "X", "", "",
+            "X", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "X", "",
+            "", "X", "",
+            "", "X", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "X",
+            "", "", "X",
+            "", "", "X"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "X", "", "",
+            "", "X", "",
+            "", "", "X"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "X",
+            "", "X", "",
+            "X", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "O", "", "",
+            "O", "", "",
+            "O", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "O", "O", "O",
+            "", "", "",
+            "", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "",
+            "O", "O", "O",
+            "", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "",
+            "", "", "",
+            "O", "O", "O"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "O", "", "",
+            "O", "", "",
+            "O", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "O", "",
+            "", "O", "",
+            "", "O", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "O",
+            "", "", "O",
+            "", "", "O"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "O", "", "",
+            "", "O", "",
+            "", "", "O"
+        ]
+        expect(is_winner(grid)).to eq(true)
+        grid = [
+            "", "", "O",
+            "", "O", "",
+            "O", "", ""
+        ]
+        expect(is_winner(grid)).to eq(true)
+    end
+    
+    it 'if noone has got a line and all spaces are filled then it is a draw so return true' do
+        grid = [
+            "O", "X", "O",
+            "X", "X", "O",
+            "O", "O", "X"
+        ]
+        expect(is_draw(grid)).to eq(true)
+    end
+end
+
+describe 'when play() is called' do
+    it 'ask the user if they want to go first' do
+        expect{ play() }.to output("Welcome to TicTacToe!! Would you like to go first? Yes or No: ").to_stdout
+    end
+
+    # it 'when user gives an answer that is not No or Yes then ask again repeating what is expected' do
+    #     play()
+    #     $stdin = "nooooo" 
+    #     expect{ play() }.to output("Try again. Would you like to go first? Yes or No: ").to_stdout
+    #     $stdin = STDIN
+    # end
+end
+
+# start with an empty grid
+# the player makes a move - X in the top-left (by entering "1")
+# what does the grid look like?
---
+++ tic_tac_toe.rb
@@ -0,0 +1,81 @@
+# The game is played on a 3x3 grid.
+
+# You are X, your opponent is O. Players take turns putting their marks in empty squares.
+
+# The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
+
+# If all 9 squares are full and no player has 3 marks in a row, the game is over.
+
+
+# def player_entry(position_number)
+#     position number = gets.to_i
+
+def play()
+    print "Welcome to TicTacToe!! Would you like to go first? Yes or No: "
+    input = gets.chomp
+    human_player_goes_first = ""
+end
+
+def initialise_grid
+    ["", "", "",
+     "", "", "",
+     "", "", ""]
+end
+
+def next_turn(player_type, grid, index)
+    # check for invalid index
+    if index < 1 || index > 9
+        puts "invalid index try a number between 0-9"
+        return
+    end
+    # determine token - this might me unneccessary as we could just pass in "X" or "O"
+    token = ""
+    if player_type == "player"
+        token = "X"
+    else
+        token = "O"
+    end
+    # check to see if index has already been used
+    if grid[index-1] != ""
+        puts "invalid index this space is already taken try again\n"
+    end
+    grid[index-1] = token
+
+    grid
+end
+
+def is_winner(grid)
+    # check top row
+    top_row = grid[0] + grid[1] + grid[2]
+    middle_row = grid[3] + grid[4] + grid[5]
+    bottom_row = grid[6] + grid[7] + grid[8]
+    left_column = grid[0] + grid[3] + grid[6]
+    middle_column = grid[1] + grid[4] + grid[7]
+    right_column = grid[2] + grid[5] + grid[8]
+    diag_top_left_to_bottom_right = grid[0] + grid[4] + grid[8]
+    diag_bottom_left_to_top_right = grid[6] + grid[4] + grid[2]
+    if top_row == "XXX" || top_row == "OOO" || 
+        middle_row == "XXX" || middle_row == "OOO" ||
+        bottom_row == "XXX" || bottom_row == "OOO" ||
+        left_column == "XXX" || left_column == "OOO" ||
+        middle_column == "XXX" || middle_column == "OOO" ||
+        right_column == "XXX" || right_column == "OOO" ||
+        diag_top_left_to_bottom_right == "XXX" || diag_top_left_to_bottom_right == "OOO" ||
+        diag_bottom_left_to_top_right == "XXX" || diag_bottom_left_to_top_right == "OOO"
+        true
+    else
+        false
+    end
+end
+
+def is_draw(grid)
+    !grid.include?("") && !is_winner(grid)
+end
+
+def print_grid
+    puts "#{get_grid[0]} | #{get_grid[1]} | #{get_grid[2]}"
+    puts "------"
+    puts "#{get_grid[3]} | #{get_grid[4]} | #{get_grid[5]}"
+    puts "------"
+    puts "#{get_grid[6]} | #{get_grid[7]} | #{get_grid[8]}"
+end

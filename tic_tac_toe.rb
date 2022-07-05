398cc88d91bba9e1976935122a861abfc9471eb6
Bobby Iveson <bobby.iveson@Bobbys-MacBook-Pro.local>
2022-07-05 11:47:55 +0100

    Draw function returns 'there is a draw' , stopped overwriting computer choice if same number was selected

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -15,13 +15,13 @@
 
 
 # To-do list:
-# 1. Fix exc eption handling if something that isnt yes or no is entered at the start of the game
-# 2. Draw game - add output to say there was a draw
-# 3. Make computer game function intelligent
-# 4. Win game while loop - take out inside checks and replace with player 1 or player 2 won, add game retart option
-# 5. Add a function that determines who the winner is
-# 6. separate into classes
-# 7. separate all clsses and corresponding tests into separate files
+
+# 3. Win game while loop - take out inside checks and replace with player 1 or player 2 won, add game retart option
+# 4. Add a function that determines who the winner is
+# 5. separate into classes
+# 6. separate all clsses and corresponding tests into separate files
+# 7. Make computer game function intelligent
+
 
 
 # ------->>>>>>>> if we separate into classes put them into different classes and put classes into different files also
@@ -36,9 +36,10 @@ class Tic_tac_toe
         "7", "8", "9"]
     end
 
-    @@available_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @@available_numbers = []
 
     def play_game
+        @@available_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         print "Welcome to TicTacToe!! Would you like to go first? Yes or No: \n \n "
         start_input = gets.chomp
         start_input = start_input.upcase!
@@ -64,10 +65,11 @@ class Tic_tac_toe
         end
 
         # once this breaks it needs to ask user if they want to play again
-        while win_game?(grid) == false || is_draw?(grid) == false
+        while !win_game?(grid) || !is_draw?(grid)
             # shouldn't check if win game is true inside this loop
             # need to make it clear who the winner is 
             # return hash that holds win = true and player
+
             if whos_turn_is_it_next == "computer"
                 whos_turn_is_it_next = "person"
                 grid = computer_play(grid)
@@ -75,6 +77,11 @@ class Tic_tac_toe
                     print_grid(grid)
                     puts "\n \n Computer Wins!! "
                     break
+                    play_game()
+                end
+                if is_draw?(grid)
+                    puts "\n It's a draw!!"
+                    play_game()
                 end
             else
                 whos_turn_is_it_next = "computer"
@@ -83,10 +90,14 @@ class Tic_tac_toe
                     print_grid(grid)
                     puts "\n \n You win!!!"
                     break
+                    play_game()
+                end
+                if is_draw?(grid)
+                    puts "\n It's a draw!!"
+                    play_game()
                 end
             end
         end
-
     end
 
     def person_play(grid)
@@ -97,10 +108,11 @@ class Tic_tac_toe
         if !@@available_numbers.include?(user_play_input) || user_play_input == nil
             puts "invalid input, try again\n"
             person_play(grid)
+        else
+            grid[user_play_input-1] = token
+            @@available_numbers.delete(user_play_input)
+            grid
         end
-        grid[user_play_input-1] = token
-        @@available_numbers.delete(user_play_input)
-        grid
     end
     # to test for next move - take grid as argument, the grid that you pass in can be in any state - create new board and put marks on the board - pass that board to the function, the function should do the right action based on the current state of the board
 
@@ -114,6 +126,18 @@ class Tic_tac_toe
         grid
     end
 
+    def determine_outcome(grid, winner)
+        if win_game?(grid)
+            print_grid(grid)
+            puts "\n \n #{winner} Wins!!"
+            break
+            play_game()
+        end
+        if is_draw?(grid)
+            puts "\n It's a draw!!"
+            play_game()
+        end
+    end
 
     def win_game?(grid)
         # name this function so you can answer with true or false, ? at the end
@@ -142,119 +166,10 @@ class Tic_tac_toe
     end
     
     def is_draw?(grid)
-        # draw_game?
-        !grid.include?("") && !win_game(grid)
+        @@available_numbers.empty? && !win_game?(grid)
     end
 end
 
-
-
-
-
-# class Tic_tac_toe
-    # @@available_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # def play()
-        
-        # print "Welcome to TicTacToe!! Would you like to go first? Yes or No: "
-        # start_input = gets.chomp
-        # start_input = start_input.upcase!
-
-        # grid = initialise_grid
-        
-        # if start_input == "YES" 
-            # puts "Please enter one of the following numbers:"
-            # print_grid(initialise_grid)
-            # user_play_input = gets.chomp.to_i
-    #         grid = next_turn("player", grid, user_play_input)
-    #         print_grid(grid)
-    #     end
-    # end
-
-    # def initialise_grid
-    #     ["1", "2", "3",
-    #     "4", "5", "6",
-    #     "7", "8", "9"]
-    # end
-
-    # def next_turn(player_type, grid, user_play_input)
-        # check for invalid index
-        # if user_play_input < 1 || user_play_input > 9
-        #     puts "invalid index try a number between 0-9"
-        #     return
-        # end
-        # determine token - this might me unneccessary as we could just pass in "X" or "O"
-        # token = ""
-        # if player_type == "player"
-        #     token = "X"
-        # else
-        #     token = "O"
-        # end
-        # check to see if index has already been used
-        # puts @@available_numbers
-        # if !@@available_numbers.include?(user_play_input)
-        #     puts "invalid index this space is already taken try again\n"
-        # end
-        # if grid[index-1] != 
-            
-        # end
-    #     grid[user_play_input-1] = token
-    #     @@available_numbers.delete(user_play_input)
-    #     grid
-    # end
-
-    # def next_turn(player_type, grid, user_play_input)
-    #     # check for invalid index
-    #     if user_play_input < 1 || user_play_input > 9
-    #         puts "invalid index try a number between 0-9"
-    #         return
-    #     end
-    #     # determine token - this might me unneccessary as we could just pass in "X" or "O"
-    #     token = ""
-    #     if player_type == "player"
-    #         token = "X"
-    #     else
-    #         token = "O"
-    #     end
-    #     # check to see if index has already been used
-    #     # puts @@available_numbers
-    #     if !@@available_numbers.include?(user_play_input)
-    #         puts "invalid index this space is already taken try again\n"
-    #     end
-    #     # if grid[index-1] != 
-            
-    #     # end
-    #     grid[user_play_input-1] = token
-    #     @@available_numbers.delete(user_play_input)
-    #     grid
-    # end
-
-    # def is_winner(grid)
-    #     # check top row
-    #     top_row = grid[0] + grid[1] + grid[2]
-    #     middle_row = grid[3] + grid[4] + grid[5]
-    #     bottom_row = grid[6] + grid[7] + grid[8]
-    #     left_column = grid[0] + grid[3] + grid[6]
-    #     middle_column = grid[1] + grid[4] + grid[7]
-    #     right_column = grid[2] + grid[5] + grid[8]
-    #     diag_top_left_to_bottom_right = grid[0] + grid[4] + grid[8]
-    #     diag_bottom_left_to_top_right = grid[6] + grid[4] + grid[2]
-    #     if top_row == "XXX" || top_row == "OOO" || 
-    #         middle_row == "XXX" || middle_row == "OOO" ||
-    #         bottom_row == "XXX" || bottom_row == "OOO" ||
-    #         left_column == "XXX" || left_column == "OOO" ||
-    #         middle_column == "XXX" || middle_column == "OOO" ||
-    #         right_column == "XXX" || right_column == "OOO" ||
-    #         diag_top_left_to_bottom_right == "XXX" || diag_top_left_to_bottom_right == "OOO" ||
-    #         diag_bottom_left_to_top_right == "XXX" || diag_bottom_left_to_top_right == "OOO"
-    #         true
-    #     else
-    #         false
-    #     end
-    # end
-
-    # def is_draw(grid)
-    #     !grid.include?("") && !is_winner(grid)
-    # end
 
     def print_grid(grid)
         puts "#{grid[0]} | #{grid[1]} | #{grid[2]}"
@@ -263,4 +178,6 @@ end
         puts "---------"
         puts "#{grid[6]} | #{grid[7]} | #{grid[8]}"
     end
-# end
+
+
+    # who's_turn_is_it = player_1 ? who's_turn_is_it = player_2 : who's_turn_is_it = player_1
